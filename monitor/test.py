51dec4a93800764c03ba9f9c747bf688a2e519e3
Kiyohito AOKI <sambar.fgfs@gmail.com>
2014-10-16 21:24:53 +0900

    Issue 2 のコメント#1に対応し、「要素数不足が起きた場合、正しいデータを受信するまでやり直す」ように修正。

--- monitor/test.py
+++ monitor/test.py
@@ -6,21 +6,29 @@ import serial
 from struct import *
 
 #Arduinoのリセット待ち
-time.sleep(2)
+time.sleep(5)
 
 #シリアルポート、ボーレート、タイムアウトを指定
 port = serial.Serial ("/dev/ttyACM0", 9600, timeout=10)
 
 while port.readline() != "3,0,0,0\n":
 #とりあえず、読んだデータを1行表示し、すぐに破棄(暫定。そのうちきちんと処理するコードが並ぶ)。
-	inputdata = port.readline(),split	# inputdataをコンマ毎に切り分ける
-		while inputdata == 4:
-			inputdata.append("0")
+	inputdata = port.readline()		# inputdataをコンマ毎に切り分ける
+	inputdata = inputdata.strip("\n")	#末尾改行を削除
+	inputdata = inputdata.split(",")
+
+	while len(inputdata) != 4:		#要素数不足の場合、要素数が合うまでやり直す
+		port.readline() 
+		inputdata = port.readline()	# inputdataをコンマ毎に切り分ける
+		inputdata = inputdata.split(",")
 
 	mcom_mode = inputdata[0]		# mcom_mode	;	mcomの状態。0=待機中、 1=ステージ1, 2=ステージ2 , 3=破壊済み
 	button_pushing = inputdata[1]	# button_pushing	;	ボタン押下中か否か
 	left = inputdata[2]				# left	;	破壊までの残り時間
+
 	disengage = inputdata[3]			# disengage	;	起動・解除までの残り時間
+
+	print mcom_mode
 	print disengage
 
 #使用済みのデータを破棄
@@ -30,4 +38,9 @@ while port.readline() != "3,0,0,0\n":
 	
 else:
 	print "M-COM was destruct."
-	exit
+	time.sleep(10)
+	port.close()				#ポートを一旦閉じる
+
+port = serial.Serial ("/dev/ttyACM0", 9600, timeout=10)		#再度シリアル接続を開始する
+print port.readline()
+exit
