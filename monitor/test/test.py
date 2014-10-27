aa3c3ec13db8ad3df3f322e55971cd5408577788
Kiyohito AOKI <sambar.fgfs@gmail.com>
2014-10-27 22:02:50 +0900

    表示改善(「起動/解除まで-1秒」の表示が出ないようにした), シリアルポート指定方法をmcom.py内での指定からmonitor.pyの最上部に移動した。

--- monitor/test/test.py
+++ monitor/test/test.py
@@ -6,6 +6,10 @@ import serial
 from struct import *
 
 
+time.sleep(2)		#Arduinoの起動待ち
+
+
+
 
 class Mcom:
 	def __init__(self, port):
@@ -14,9 +18,9 @@ class Mcom:
 		self.left = ""
 		self.disengage = "" 
 
-		self.port = serial.Serial (port, 9600, timeout=10)
+		self.port = serial.Serial (port, 9600, timeout=10)	#シリアルポート、ボーレート、タイムアウトを指定
 
-	def reset(self, port):
+	def reset(self,port):
 		self.port.close()				#ポートを一旦閉じる
 		self.port = serial.Serial (port, 9600, timeout=10)	#再度開ける
 		return
@@ -32,7 +36,7 @@ class Mcom:
 
 	def getdata(self):
 		indata = self.checkdata()
-		self.mcom_mode = int(indata[0])			# mcom_mode	;	mcomの状態。0=待機中、 1=ステージ1, 2=ステージ2 , 3=破壊済み
+		self.mcom_mode = int(indata[0])		# mcom_mode	;	mcomの状態。0=待機中、 1=ステージ1, 2=ステージ2 , 3=破壊済み
 		self.button_pushing = int(indata[1])		# button_pushing	;	ボタン押下中か否か
 
 		if  int(indata[2]) % 1000 >= 1 : 				# left	;	破壊までの残り時間
@@ -40,11 +44,8 @@ class Mcom:
 		else:
 			self.left  = str( int(indata[2]) /1000)
 
-
-		if int(indata[3]) <= 1000 :					# disengage	;	起動・解除までの残り時間	
-			self.disengage = str(1)					#1000ミリ秒以下なら1秒を表示
-		elif  int(indata[3]) % 1000 >= 1 : 				
-			self.disengage = str (( int(indata[3]) +1000) /1000 )		#余りがあれば、表示は + 1000ミリ秒
+		if  int(indata[3]) % 1000 >= 1 : 				# disengage	;	起動・解除までの残り時間	
+			self.disengage = str( (int(indata[3]) +1000) /1000)		#余りがあれば、表示は + 1000ミリ秒
 		else:
 			self.disengage = str( int(indata[3]) /1000)
 		return
